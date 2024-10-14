a1aa7929bdb93c3176f06b1b141258cf12f50c29
Jmumo <48655696+Jmumo@users.noreply.github.com>
2024-10-14 12:49:55 +0300

    Update isHappyclass.java

--- isHappyclass.java
+++ isHappyclass.java
@@ -1,1 +1,34 @@
 
+import java.util.Set;
+import java.util.HashSet;
+
+class isHappyclass {
+    public static void main(String[] args) {
+        System.out.println(isHappy(82));
+    }
+    
+    public static boolean isHappy(int n){
+        
+        Set<Integer> usedNums = new HashSet<>();
+        
+        while(true){
+            int sum = 0;
+            
+            while(n != 0){
+                sum += Math.pow(n % 10, 2.0);
+                n = n / 10;
+            }
+            n = sum;
+            
+            if (sum == 1){
+                return true;
+            }
+            
+            if (usedNums.contains(n)){
+                return false;
+            }else{
+                usedNums.add(n);
+            }
+        }
+    }
+}
